479521e340467414ef875eda24bd9a28218d3866
Jeremy Hotz <JeremyHotz@Jeremys-iMac-2.local>
2021-06-25 17:24:16 -0600

    connect to mongoose databse

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var session = require('express-session');
+// var passport = require('passport');
+// var methodOverride = require('method-override');
+
+// Process the .env VERY early
+require('dotenv').config();
+
+require('./config/database');
+// require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
