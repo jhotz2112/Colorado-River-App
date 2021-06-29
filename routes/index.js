75b05a6bde8a5c7531d41b8776cfb4144c8d4205
Jeremy Hotz <JeremyHotz@Jeremys-iMac-2.local>
2021-06-29 17:22:58 -0600

    add background

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Colorado Yak' });
+  res.render('index', { title: 'Get `Yakkin ' });
 });
 
 router.get('/auth/google', passport.authenticate(
